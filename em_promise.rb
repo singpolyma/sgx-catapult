db2bc83cb43665a8bc561bf0576cb2bd41688d2b
Stephen Paul Weber <singpolyma@singpolyma.net>
2017-03-16 18:15:41 -0500

    First HTTP request using em-http

---
+++ em_promise.rb
@@ -0,0 +1,40 @@
+require "eventmachine"
+require "promise"
+
+class EMPromise < Promise
+	def initialize(deferrable=nil)
+		super()
+		fulfill(deferrable) if deferrable
+	end
+
+	def fulfill(value, bind_defer=true)
+		if bind_defer && value.is_a?(EM::Deferrable)
+			value.callback { |x| fulfill(x, false) }
+			value.errback(&method(:reject))
+		else
+			super(value)
+		end
+	end
+
+	def defer
+		EM.next_tick { yield }
+	end
+
+	def self.reject(e)
+		new.tap { |promise| promise.reject(e) }
+	end
+end
+
+module EventMachine
+	module Deferrable
+		def promise
+			EMPromise.new(self)
+		end
+
+		[:then, :rescue, :catch].each do |method|
+			define_method(method) do |*args, &block|
+				promise.public_send(method, *args, &block)
+			end
+		end
+	end
+end

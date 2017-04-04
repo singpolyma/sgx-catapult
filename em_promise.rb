c25ea8c65f177fd8b8b0b75965914a887d8c1690
Denver Gingerich <denver@ossguy.com>
2017-04-04 18:55:36 +0000

    add requisite copyright notice for a98b0f9 changes

--- em_promise.rb
+++ em_promise.rb
@@ -1,3 +1,22 @@
+#!/usr/bin/env ruby
+#
+# Copyright (C) 2017  Stephen Paul Weber <singpolyma@singpolyma.net>
+#
+# This file is part of sgx-catapult.
+#
+# sgx-catapult is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# sgx-catapult is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with sgx-catapult.  If not, see <http://www.gnu.org/licenses/>.
+
 require "eventmachine"
 require "promise"
 
